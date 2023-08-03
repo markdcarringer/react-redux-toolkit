538044a94e4fe6a8956440366d71fdbb5b765246
mic <mic@ornl.gov>
2023-08-03 14:34:42 -0400

    Added navigation bar. (32:33)

--- src/components/RootLayout.js
+++ src/components/RootLayout.js
@@ -1,9 +1,10 @@
 import { Outlet } from 'react-router-dom';
+import NavBar from './NavBarPanel';
 
 const RootLayout = () => {
     return (
         <>
-            <h1>Navigation</h1>
+            <NavBar />
             <main>
                 <Outlet />
             </main>
