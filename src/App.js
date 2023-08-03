69fe5bde2e8b84b9aa31b653901927206c86f425
mic <mic@ornl.gov>
2023-08-03 14:13:15 -0400

    Completed URL routing.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
-import Product from './components/Product';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
+import Cart from './components/Cart';
+import Dashboard from './components/Dashboard';
+import RootLayout from './components/RootLayout';
 
 function App() {
+
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Dashboard />}></Route>
+      <Route path="/cart" element={<Cart />}></Route>
+    </Route>
+  ));
+
   return (
     <div className="App">
-      <Product />
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/components/RootLayout.js
@@ -0,0 +1,14 @@
+import { Outlet } from 'react-router-dom';
+
+const RootLayout = () => {
+    return (
+        <>
+            <h1>Navigation</h1>
+            <main>
+                <Outlet />
+            </main>
+        </>
+    )
+}
+
+export default RootLayout;
