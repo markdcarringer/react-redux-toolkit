50558387e8ea7a3bb4c3cd4d244ad81719da21b1
mic <mic@ornl.gov>
2023-08-07 10:14:50 -0400

    Fixed 'Each child should have a unique key' warning.

--- src/components/Product.js
+++ src/components/Product.js
@@ -13,8 +13,8 @@ const Product = () => {
     }, []);
 
     const cards = products.map(product =>
-        <div className="col-md-3" style={{ marginBottom: "10px" }}>
-            <Card key={product.id} className="h-100" style={{ width: '18rem' }}>
+        <div key={product.id} className="col-md-3" style={{ marginBottom: "10px" }}>
+            <Card className="h-100" style={{ width: '18rem' }}>
                 <div className="text-center">
                     <Card.Img variant="top" src={product.image} style={{ width: '100px', height: '130px' }} />
                 </div>
